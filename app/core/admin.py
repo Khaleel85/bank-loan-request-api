4b3e01bdbd0cf10a54186c7371bc50a415c303b4
Khaleel <global-config@example.com>
2023-06-08 10:04:39 +0300

    Added groups and permissions to the user

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,41 +3,49 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core import models
+
+
 class UserAdmin(BaseUserAdmin):
-    ordering = ['id']
-    list_display = ['email', 'name']
+    ordering = ["id"]
+    list_display = ["email", "name"]
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {"fields": ("email", "password")}),
         (
-            _('Permissions'),
+            _("Permissions"),
             {
-                'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
                 )
-            }
+            },
         ),
-        (_('Important dates'), {'fields': ('last_login', )}),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
-    readonly_fields = ['last_login']
+    readonly_fields = ["last_login"]
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'email',
-                'password1',
-                'password2',
-                'name',
-                'position',
-                'phone',
-                'track',
-                'is_active',
-                'is_staff',
-                'is_superuser',
-            )
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "position",
+                    "phone",
+                    "track",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
     )
+
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Investigation)
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext_lazy as _
 
@@ -6,54 +5,60 @@ from rest_framework import serializers
 from investigation.serializers import RegionSerializer
 from core.models import Region
 
+
 class UserSerializer(serializers.ModelSerializer):
-  track = serializers.SlugRelatedField(slug_field='region', queryset=Region.objects.all(), required=True)
+    track = serializers.SlugRelatedField(
+        slug_field="region", queryset=Region.objects.all(), required=True
+    )
 
-  class Meta:
-    model = get_user_model()
-    fields = ['email', 'password', 'name', 'phone', 'position', 'track']
-    extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+    class Meta:
+        model = get_user_model()
+        fields = ["email", "password", "name", "phone", "position", "track"]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
-  def create(self, validated_data):
-    track_names = validated_data.pop('track')
-    try:
-      track = Region.objects.get(region=track_names.strip())
-    except Region.DoesNotExist:
-      raise serializers.ValidationError({'track': f'Region {track_names} does not exist.'})
-    except Region.MultipleObjectsReturned:
-      raise serializers.ValidationError({'track': f'Multiple regions with the name {track_names} exist.'})
-    user = get_user_model().objects.create_user(track=track, **validated_data)
-    return user
+    def create(self, validated_data):
+        track_names = validated_data.pop("track")
+        try:
+            track = Region.objects.get(region=track_names.strip())
+        except Region.DoesNotExist:
+            raise serializers.ValidationError(
+                {"track": f"Region {track_names} does not exist."}
+            )
+        except Region.MultipleObjectsReturned:
+            raise serializers.ValidationError(
+                {"track": f"Multiple regions with the name {track_names} exist."}
+            )
+        user = get_user_model().objects.create_user(track=track, **validated_data)
+        return user
 
-  def update(self, instance, validated_data):
-    password = validated_data.pop('password', None)
-    user = super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
 
-    if password:
-      user.set_password(password)
-      user.save()
+        if password:
+            user.set_password(password)
+            user.save()
 
-    return user
-
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(
-        style={'input_type': 'password'},
+        style={"input_type": "password"},
         trim_whitespace=False,
     )
 
     def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
+        email = attrs.get("email")
+        password = attrs.get("password")
         user = authenticate(
-            request=self.context.get('request'),
+            request=self.context.get("request"),
             username=email,
             password=password,
         )
         if not user:
-            msg = _('Unable to authenticate with provided credentials')
-            raise serializers.ValidationError(msg, code='authorization')
-        attrs['user'] = user
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code="authorization")
+        attrs["user"] = user
         return attrs
